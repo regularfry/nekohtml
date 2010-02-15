ac9b10c478fefb6ddffa1acfd84fb131fb9976e2
Alex Young <alex@blackkettle.org>
2010-02-15 09:12:33 +0000

    Missed a file from the previous patch

--- lib/nekohtml.rb
+++ lib/nekohtml.rb
@@ -2,21 +2,34 @@ require 'celerity'
 require 'nekohtml/html_document'
 
 module Nekohtml
-  def Nekohtml.parse(string)
-    if string
-      jparser = org.cyberneko.html.parsers.DOMParser.new
+  class << self
+
+    def parser()
+      configuration = org.cyberneko.html.HTMLConfiguration.new
+      jparser = org.apache.xerces.parsers.DOMParser.new(configuration)
+      jparser.setProperty("http://cyberneko.org/html/properties/names/elems", "lower");
       jparser.setFeature("http://xml.org/sax/features/namespaces", false)
+      return jparser
+    end
 
-      jinput_reader = java.io.StringReader.new(string.to_java_string)
-      jinput_source = org.xml.sax.InputSource.new(jinput_reader)
-      jparser.parse(jinput_source)
-      jdocument = jparser.get_document()
-      # We know that the document has successfully been parsed 
-      # at this point.
+    # Parse the string. case_sensitive controls whether you can use lower-case xpath
+    # elements for tag names or not. case_sensitive=true uses the default NekoHTML
+    # parser, which forces everything to be upper case per HTML 4.01. This is a pain.
+    def parse(string)
+      if string
+        jparser = parser()
 
-      return HtmlDocument.new(jdocument)
-    else
-      raise ArgumentError.new
+        jinput_reader = java.io.StringReader.new(string.to_java_string)
+        jinput_source = org.xml.sax.InputSource.new(jinput_reader)
+        jparser.parse(jinput_source)
+        jdocument = jparser.get_document()
+        # We know that the document has successfully been parsed 
+        # at this point.
+
+        return HtmlDocument.new(jdocument)
+      else
+        raise ArgumentError.new
+      end
     end
   end
 end
